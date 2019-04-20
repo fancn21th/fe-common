f1689d6a1031dded6d0f4f2e6591acb4d9648fdb
alex.fan <alex.fan@okchem.com>
2019-04-20 11:35:08 +0800

    fix an react warning level error

--- src/components/Form/Common/TextField.js
+++ src/components/Form/Common/TextField.js
@@ -1,21 +1,49 @@
-import React from 'react'
-import {Input, Form} from 'antd';
+import React from "react";
+import { Input, Form } from "antd";
 const FormItem = Form.Item;
-const TextField = ({input, label, hasFeedback, labelSpan,okBlur, okFocus,okChange,wrapperSpan, showRequiredStar, meta: {asyncValidating, touched, error}, ...custom}) => {
-
+const TextField = ({
+  input,
+  label,
+  hasFeedback,
+  labelSpan,
+  okBlur,
+  okFocus,
+  okChange,
+  wrapperSpan,
+  showRequiredStar,
+  meta: { asyncValidating, touched, error },
+  ...custom
+}) => {
   return (
-
     <FormItem
       label={label}
-      labelCol={{span: labelSpan == undefined ? "8" : labelSpan}}
-      wrapperCol={{span: wrapperSpan == undefined ? "16" : wrapperSpan}}
-      hasFeedback={(hasFeedback==undefined||hasFeedback==true)?true:false}
-      validateStatus={asyncValidating ? "validating" : ((touched && error) ? "error" : "")}
-      help={touched && error} required={showRequiredStar ? true : false}
+      labelCol={{ span: labelSpan == undefined ? "8" : labelSpan }}
+      wrapperCol={{ span: wrapperSpan == undefined ? "16" : wrapperSpan }}
+      hasFeedback={
+        hasFeedback == undefined || hasFeedback == true ? true : false
+      }
+      validateStatus={
+        asyncValidating ? "validating" : touched && error ? "error" : ""
+      }
+      help={touched && error}
+      required={showRequiredStar ? true : false}
     >
-      <Input {...input} size="default" {...custom} onChange={(e) => { input.onChange(e);okChange&&okChange(e) } } onblur={(e)=>{okBlur&&okBlur(e)}} onfocus={(e)=>{okFocus&&okFocus(e)}}   />
+      <Input
+        {...input}
+        size="default"
+        {...custom}
+        onChange={e => {
+          input.onChange(e);
+          okChange && okChange(e);
+        }}
+        onBlur={e => {
+          okBlur && okBlur(e);
+        }}
+        onFocus={e => {
+          okFocus && okFocus(e);
+        }}
+      />
     </FormItem>
-  )
-
-}
-export default TextField
+  );
+};
+export default TextField;
